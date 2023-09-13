75509e2f5a4c0c6dad4f487d87f737991839fa90
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-09-13 13:43:43 +0330

    feat: custom admin panel for user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from accounts.forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from accounts.models import (
+    User as UserModel,
+)
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+
+    list_display = ('email', 'full_name', 'is_author', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('full_name', 'profile_user')}),
+        ('Permissions', {'fields': ('is_admin', 'is_author', 'is_active', 'is_superuser')}),
+    )
+   
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'full_name', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', '')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(UserModel, UserAdmin)
