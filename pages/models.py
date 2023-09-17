6b7d41eac3297b842c0c6a0186741fefd3814238
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-09-17 18:41:46 +0330

    feat: add contact us model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from utils.date_conversion.utils import jajali_converter
 
-# Create your models here.
+from django.utils.translation import gettext as _
+
+
+class ContactUs(models.Model):
+    title = models.CharField(max_length=155, verbose_name=_('موضوع'))
+    email = models.EmailField(max_length=255, verbose_name=_('ایمیل'))
+    text = models.TextField(verbose_name=_('متن'))
+    is_check = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('تاریخ ایجاد'))
+
+    def __str__(self):
+        return f'{self.email} - {self.title}'
+    
+    def get_jalali_date(self):
+        return jajali_converter(self.created_at)
+    get_jalali_date.short_description = 'تاریخ عضویت'
